5284926d65e8a5922d03c5c2bf207f0452e69c22
SM Mashuque <smjim1998@gmail.com>
2018-12-30 03:35:33 -0500

    get backend api delete to work with button click on frontend.

--- notes/urls.py
+++ notes/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import NotesListView
+from .views import NotesListView, NotesDeleteView
 
 urlpatterns = [
     path('', NotesListView.as_view()),
+    path('<pk>/delete', NotesDeleteView.as_view()),
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, DestroyAPIView
 from .models import Note
 from .serializer import NoteSerializer
 
 # Create your views here.
 class NotesListView(ListCreateAPIView):
+    queryset = Note.objects.all()
+    serializer_class = NoteSerializer
+
+
+class NotesDeleteView(DestroyAPIView):
     queryset = Note.objects.all()
     serializer_class = NoteSerializer
